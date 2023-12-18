86696e1ee76ce85dc2ceb1f7e8d2a16d3b81d78e
Ealse de Wilde <ealsedewilde@gmail.com>
2023-12-18 22:26:31 +0100

    simplied uniqueness check

--- frontend-ccnl/src/main/java/nl/ealse/ccnl/LedenadministratieApplication.java
+++ frontend-ccnl/src/main/java/nl/ealse/ccnl/LedenadministratieApplication.java
@@ -2,8 +2,16 @@ package nl.ealse.ccnl;
 
 import javafx.application.Application;
 
+/**
+ * Main entrypoint of the application.
+ */
 public class LedenadministratieApplication {
 
+  /**
+   * Start the single instance of this application.
+   *
+   * @param args - no args needed
+   */
   public static void main(String[] args) {
     UniqueCheck check = new UniqueCheck();
     if (check.uniqueProcess()) {
--- frontend-ccnl/src/main/java/nl/ealse/ccnl/UniqueCheck.java
+++ frontend-ccnl/src/main/java/nl/ealse/ccnl/UniqueCheck.java
@@ -1,57 +1,57 @@
 package nl.ealse.ccnl;
 
 import java.util.Optional;
-import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Check if there is no other instance of this application running.
+ * This check will work for the way this application is installed by it's MS installer.
+ */
 public class UniqueCheck {
 
   private boolean nonUnique;
 
+  /**
+   * Determine uniqueness of the application.
+   *
+   * @return true when unique
+   */
   public boolean uniqueProcess() {
-    ProcessData currentData = new ProcessData(ProcessHandle.current());
-    long pid = currentData.getPid();
+    long start = System.currentTimeMillis();
+    ProcessHandle current = ProcessHandle.current();
+    String refCommand = getCommand(current);
+    long refPid = current.pid();
+
     ProcessHandle.allProcesses().forEach(process -> {
-      ProcessData processData = new ProcessData(process, currentData.getCommand());
-      long descendentPid = processData.getDescendentPid();
-      if (descendentPid != 0L && pid != descendentPid) {
-        nonUnique = true;
+      // The current process is a child of a process with the same command String.
+      // We check all processes with the same command String in the process and its parent.
+      // With one instance running it will be the same process as the current process, 
+      // (both pid's match).
+      // When the pid's differ, we have found that there is another instance 
+      //of this application running.
+      if (refCommand.equals(getCommand(process)) && refCommand.equals(getParentCommand(process))) {
+        if (refPid != process.pid()) {
+          nonUnique = true;
+        }
       }
     });
     return !nonUnique;
   }
 
-  @Getter
-  private static class ProcessData {
-    private long pid;
-    private String command;
-    private long descendentPid;
-
-    private ProcessData(ProcessHandle process) {
-      this.pid = process.pid();
-      this.command = getCommand(process);
+  private String getCommand(ProcessHandle process) {
+    Optional<String> commandOptional = process.info().command();
+    if (commandOptional.isPresent()) {
+      return commandOptional.get();
     }
+    return "";
+  }
 
-    private ProcessData(ProcessHandle process, String command) {
-      this.pid = process.pid();
-      this.command = command;
-      initialize(process);
+  private String getParentCommand(ProcessHandle process) {
+    Optional<ProcessHandle> parent = process.parent();
+    if (parent.isPresent()) {
+      return getCommand(parent.get());
     }
-
-    private void initialize(ProcessHandle process) {
-      process.descendants().forEach(descendent -> {
-        if (getCommand(descendent).equals(command)) {
-          descendentPid = descendent.pid();
-        }
-      });
-    }
-
-    private String getCommand(ProcessHandle process) {
-      Optional<String> commandOptional = process.info().command();
-      if (commandOptional.isPresent()) {
-        return commandOptional.get();
-      }
-      return "";
-    }
+    return null;
   }
 
 }
