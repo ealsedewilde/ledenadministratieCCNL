22a641c976b319ba24c57a32acd2ef1ff9a37610
Ealse de Wilde <ealsedewilde@gmail.com>
2024-01-03 12:12:36 +0100

    xxx

--- frontend-ccnl/src/main/java/nl/ealse/ccnl/UniqueCheck.java
+++ frontend-ccnl/src/main/java/nl/ealse/ccnl/UniqueCheck.java
@@ -3,8 +3,8 @@ package nl.ealse.ccnl;
 import java.util.Optional;
 
 /**
- * Check if there is no other instance of this application running.
- * This check will work for the way this application is installed by it's MS installer.
+ * Check if there is no other instance of this application running. This check will work for the way
+ * this application is installed by it's MS installer.
  */
 public class UniqueCheck {
 
@@ -23,14 +23,13 @@ public class UniqueCheck {
     ProcessHandle.allProcesses().forEach(process -> {
       // The current process is a child of a process with the same command String.
       // We check all processes with the same command String in the process and its parent.
-      // With one instance running it will be the same process as the current process, 
+      // With one instance running it will be the same process as the current process,
       // (both pid's match).
-      // When the pid's differ, we have found that there is another instance 
-      //of this application running.
-      if (refCommand.equals(getCommand(process)) && refCommand.equals(getParentCommand(process))) {
-        if (refPid != process.pid()) {
-          nonUnique = true;
-        }
+      // When the pid's differ, we have found that there is another instance
+      // of this application running.
+      if (refCommand.equals(getCommand(process)) && refCommand.equals(getParentCommand(process))
+          && refPid != process.pid()) {
+        nonUnique = true;
       }
     });
     return !nonUnique;
