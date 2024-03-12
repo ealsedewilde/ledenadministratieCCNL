b959e65ed0ab3cd2e5ec1208a8a502f1dc7de876
Ealse de Wilde <ealsedewilde@gmail.com>
2024-03-12 15:36:36 +0100

    xxx

--- services-ccnl/src/test/java/nl/ealse/ccnl/service/SepaDirectDebitServiceTest.java
+++ services-ccnl/src/test/java/nl/ealse/ccnl/service/SepaDirectDebitServiceTest.java
@@ -24,7 +24,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 class SepaDirectDebitServiceTest {
   
