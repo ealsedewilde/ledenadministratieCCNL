85b8fc9b45c387bb5197b75a554f34ede077486f
Ealse de Wilde <ealsedewilde@gmail.com>
2024-04-06 20:32:19 +0200

    xxx

--- annual-ccnl/src/test/java/nl/ealse/ccnl/ledenadministratie/payment/strategy/MemberStrategieTest.java
+++ annual-ccnl/src/test/java/nl/ealse/ccnl/ledenadministratie/payment/strategy/MemberStrategieTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import nl.ealse.ccnl.ledenadministratie.model.Member;
 import nl.ealse.ccnl.ledenadministratie.payment.IngBooking;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.w3c.dom.Element;
@@ -18,23 +19,36 @@ class MemberStrategieTest {
   
   @Test
   void testNaam() {
-    members.add(member);
-    member.setLastName("Essen");
-    member.setInitials("G.R.");
-    member.setMemberNumber(1499);
     IngBooking booking = new TestBooking("ESSEN G R VAN");
     sut.bepaalLidnummer(booking);
     Assertions.assertEquals(1499, booking.getLidnummer());
   }
   @Test
   void testNaam2() {
+    IngBooking booking = new TestBooking("HR G R VAN ESSEN EN/OFMW I EIKEN");
+    sut.bepaalLidnummer(booking);
+    Assertions.assertEquals(1499, booking.getLidnummer());
+  }
+  @Test
+  void testNaam3() {
+    Member m2 = new Member();
+    members.add(m2);
+    m2.setLastName("Essen");
+    m2.setInitials("A.R.");
+    m2.setMemberNumber(1498);
+    IngBooking booking = new TestBooking("HR G R VAN ESSEN EN/OFMW I EIKEN");
+    sut.bepaalLidnummer(booking);
+    Assertions.assertEquals(1499, booking.getLidnummer());
+  }
+  
+  @BeforeEach
+  void setup() {
+    members.clear();
     members.add(member);
     member.setLastName("Essen");
     member.setInitials("G.R.");
     member.setMemberNumber(1499);
-    IngBooking booking = new TestBooking("HR G R VAN ESSEN EN/OFMW I EIKEN");
-    sut.bepaalLidnummer(booking);
-    Assertions.assertEquals(1499, booking.getLidnummer());
+    
   }
   
   private static class TestBooking extends IngBooking {
